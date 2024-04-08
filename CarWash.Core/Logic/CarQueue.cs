c5b7d8e487f8c94a1df3eeac0f340edf013ab34a
BreadDogee <roma.ponomarev.05@mail.ru>
2024-04-08 15:45:24 +0600

    Добавление очереди и логики поста автомойки

---
+++ CarWash.Core/Logic/CarQueue.cs
@@ -0,0 +1,20 @@
+﻿using CarWash.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash.Core.Logic
+{
+    public class CarQueue
+    {
+        private readonly Queue<ICar> _queue = new();
+
+        public void Enqueue(ICar car) => _queue.Enqueue(car);
+        public ICar? Dequeue() => _queue.Count > 0 ? _queue.Dequeue() : null;
+        public bool HasCars => _queue.Count > 0;
+        public int Count => _queue.Count;
+    }
+
+}
---
+++ CarWash.Core/Logic/CarWashSlot.cs
@@ -0,0 +1,39 @@
+﻿using CarWash.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash.Core.Logic
+{
+    public class CarWashSlot
+    {
+        public ICar? CurrentCar { get; private set; }
+        public int TimeRemaining { get; private set; } = 0;
+
+        public bool IsFree => TimeRemaining == 0;
+
+        public void AssignCar(ICar car)
+        {
+            CurrentCar = car;
+            TimeRemaining = car.WashTime;
+        }
+
+        public int Tick()
+        {
+            if (TimeRemaining > 0)
+            {
+                TimeRemaining--;
+                if (TimeRemaining == 0)
+                {
+                    CurrentCar = null;
+                }
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+
+}
