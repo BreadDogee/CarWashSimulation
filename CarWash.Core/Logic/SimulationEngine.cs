9d72569dac556d8e791e53410eef8c770d4e7b59
BreadDogee <roma.ponomarev.05@mail.ru>
2024-04-09 19:25:15 +0600

    Добавление движка симуляции

---
+++ CarWash.Core/Logic/SimulationEngine.cs
@@ -0,0 +1,43 @@
+﻿using CarWash.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWash.Core.Logic
+{
+    public class SimulationEngine
+    {
+        private readonly CarWashService _carWash;
+        private readonly Func<int, IEnumerable<ICar>> _carGenerator;
+
+        public SimulationEngine(int slotCount, Func<int, IEnumerable<ICar>> carGenerator)
+        {
+            _carWash = new CarWashService(slotCount);
+            _carGenerator = carGenerator;
+        }
+
+        public int TotalEarnings => _carWash.TotalEarnings;
+        public int TotalCarsServed => _carWash.TotalCarsServed;
+
+        public void Run(int minutes)
+        {
+            for (int i = 0; i < minutes; i++)
+            {
+                foreach (var car in _carGenerator(i))
+                {
+                    _carWash.AddCar(car);
+                }
+
+                _carWash.Tick();
+            }
+
+            while (_carWash.IsProcessing)
+            {
+                _carWash.Tick();
+            }
+        }
+    }
+
+}
